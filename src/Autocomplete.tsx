a91623b5489276703ed9f6c59b2ef58be42dd446
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2024-10-10 22:41:32 +1100

    useEffect hook used and update in the API parameter

--- src/Autocomplete.tsx
+++ src/Autocomplete.tsx
@@ -1,41 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Autocomplete = () => {
   const [inputValue, setInputValue] = useState<string>("");
-  const [names, setUserNames] = useState<string[]>([]);
+  const [userNames, setUserNames] = useState<string[]>([]);
   const [suggestions, setSuggestions] = useState<string[]>([]);
   const [showSuggestions, setShowSuggestions] = useState<boolean>(false);
 
   const handleInputeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
-    fetchData();
+  };
 
-    if (inputValue.length > 0 && names.length > 0) {
-      const filteredNames: string[] = names.filter((name) =>
-        name.toLowerCase().includes(inputValue.toLowerCase())
-      );
-      setSuggestions(filteredNames);
+  useEffect(() => {
+    const fetchData = async () => {
+      if (inputValue.length === 0) {
+        setUserNames([]);
+        return;
+      }
+      try {
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/users?name_like=${inputValue}`
+        );
+        const data = await response.json();
+        const userNames: string[] = data.map(
+          (user: { username: string }) => user.username
+        );
+
+        setUserNames(userNames);
+      } catch (e) {
+        console.log("ERROE: ", e);
+      }
+    };
+    fetchData();
+  }, [inputValue]);
+
+  useEffect(() => {
+    if (inputValue.length > 0 && userNames.length > 0) {
+      setSuggestions(userNames);
       setShowSuggestions(true);
     } else {
       setShowSuggestions(false);
       setSuggestions([]);
     }
-  };
-
-  const fetchData = async () => {
-    try {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/users"
-      );
-      const data = await response.json();
-      const userNames: string[] = data.map(
-        (user: { username: string }) => user.username
-      );
-      setUserNames(userNames);
-    } catch (e) {
-      console.log("ERROE: ", e);
-    }
-  };
+  }, [inputValue, userNames]);
 
   const handleSelect = (suggestion: string) => {
     setInputValue(suggestion);
